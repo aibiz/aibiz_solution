7c2e0c6c8dbd631b201e82c9a74251d16c4dcedd
qudwn1114 <qudwn1114@gmail.com>
2021-08-26 14:06:30 +0900

    Update Login

--- monitoring/views/monitoring_views.py
+++ monitoring/views/monitoring_views.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 class MoniterView(LoginRequiredMixin, View):
+    login_url = '/login'
     def get(self, request: HttpRequest, *args, **kwargs):
         context = {}
         context['username'] = request.user.username

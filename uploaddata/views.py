1a3508071a02b2b472e16324d4fcfa917d2f56bf
qudwn1114 <qudwn1114@gmail.com>
2021-08-26 17:43:54 +0900

    Update models.py

--- uploaddata/views.py
+++ uploaddata/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, JsonResponse
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+
+class DataUploadView(LoginRequiredMixin, View):
+    login_url = '/login'
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+        context['username'] = request.user.username
+        
+        return render(request, 'dataupload.html', context)

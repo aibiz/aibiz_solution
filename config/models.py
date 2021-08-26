be65e18e5318e9cf3e15922bef35f74eb02aa978
qudwn1114 <qudwn1114@gmail.com>
2021-08-26 13:28:47 +0900

    Update Register

--- config/models.py
+++ config/models.py
@@ -8,6 +8,7 @@ class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     phone_no = models.CharField(db_column='phone_no', null=True, max_length=100, default='')
     usage_flag = models.CharField(max_length=10, default='1')
+    name = models.CharField(db_column='name', max_length=100)
 
     class Meta:
         db_table = "monitoring_profile"
--- monitoring/views/user_views.py
+++ monitoring/views/user_views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+from django.contrib.auth.models import User
+from config.models import profile
 
 class LoginView(View):
 
@@ -25,3 +27,37 @@ class RegisterView(View):
         context = {}
 
         return render(request, 'user/register.html', context)
+
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+        id = request.POST['card-id']
+        password = request.POST['card-password']
+        password_confirm = request.POST['card-confirm-password']
+        name = request.POST['card-name']
+        email = request.POST['card-email']
+
+        if password != password_confirm:
+            context['success'] = False
+            context['message'] = '비밀번호가 일치하지 않습니다.'
+            return JsonResponse(context, content_type='application/json')
+
+        try:
+            user = User.objects.get(username=id)
+            context['success'] = False
+            context['message'] = '아이디가 이미 존재합니다.'
+            return JsonResponse(context, content_type='application/json')
+
+        except:
+            user = User.objects.create_user(
+                id,
+                email,
+                password
+            )
+            userid = user.id
+            rsProfile = profile.objects.get(user_id=userid)
+            rsProfile.name = name
+            rsProfile.save()
+
+        context['success'] = True
+        context['message'] = '등록 되었습니다.'
+        return JsonResponse(context, content_type='application/json')

84564ed6753d718bfd800c4b946ee36d93e33cdb
qudwn1114 <qudwn1114@gmail.com>
2021-08-26 14:02:34 +0900

    Update Login

--- monitoring/views/monitoring_views.py
+++ monitoring/views/monitoring_views.py
@@ -1,5 +1,12 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, JsonResponse
+
 from django.shortcuts import render, redirect, get_object_or_404
 
-class MoniterView(TemplateView):
-    template_name = 'index.html'
+class MoniterView(LoginRequiredMixin, View):
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+        context['username'] = request.user.username
+        
+        return render(request, 'index.html', context)

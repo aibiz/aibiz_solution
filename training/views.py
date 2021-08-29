7c57fc66c9ed58e0fc907a2064d00a48a11b3a05
kerlic <kerli@naver.com>
2021-08-29 19:29:27 +0900

    Update training

--- training/views.py
+++ training/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from config.models import mmDataset
 
 def training_main(request):
-    context ={}
+    context = {}
 
+    datalist = mmDataset.objects.all().order_by('-id')[:30]
+    context['dataList'] = datalist
+    print(datalist)
     return render(request, 'training.html', context)
+
+
+# def traindata_select():
+
+
+
 # Create your views here.

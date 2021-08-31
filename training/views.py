9f5a0562b4845363c9d4005528b25f985cd96ddf
kerlic <kerli@naver.com>
2021-08-31 13:54:51 +0900

    Update training

--- training/views.py
+++ training/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from config.models import mmDataset
+# from aiengine.learning_code import learn_anomaly
 
 def training_main(request):
     context = {}
 
-    datalist = mmDataset.objects.all().order_by('-id')[:30]
-    context['dataList'] = datalist
-    print(datalist)
+    trainDataList = mmDataset.objects.all().order_by('-id')[:50]
+    testDataList = mmDataset.objects.all().order_by('-id')[:50]
+    context['trainDataList'] = trainDataList
+    context['testDataList'] = testDataList
+    print(trainDataList)
     return render(request, 'training.html', context)
 
 
-# def traindata_select():
+def start_training(request):
+    print(request)
 
 
 

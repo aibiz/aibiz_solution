9abb90f2efe87718d632de1a2c7d1a21b86b3f5b
qudwn1114 <qudwn1114@gmail.com>
2021-09-02 17:46:03 +0900

    Update Data Upload

--- static/jsmeta/uploaddata/script_uploaddata.js
+++ static/jsmeta/uploaddata/script_uploaddata.js
@@ -1,12 +1,30 @@
-function upload(){
+function showModal(){
     var file = document.getElementById('file');
+    var extension = file.value.split('.').pop().toLowerCase();
     if(file.value == ''){
         file.focus();
         return false;
     }
+    if(extension != 'zip'){
+        file.focus();
+        alert('zip파일을 올려주세요.');
+        return false;
+    }
+    $('#directory-modal').modal('show');
+}
+
+function upload(){
+    var directory_name = document.getElementById('directory-name');
+    if(directory_name.value == ''){
+        directory_name.focus();
+        return false;
+    }
+
     var url = $("#UploadForm").attr("action");
     var form = $('#UploadForm')[0]; 
     var formData = new FormData(form);
+    formData.append('directory-name', directory_name.value);
+
     $.ajax({
         type : 'POST',
         url : url,
--- uploaddata/views.py
+++ uploaddata/views.py
@@ -7,6 +7,19 @@ from config.models import mmDataset
 from django.shortcuts import render, redirect, get_object_or_404
 
 import os
+import zipfile
+
+def createDirectory(directory): 
+    try: 
+        if not os.path.exists(directory): 
+            os.makedirs(directory)
+            return True
+        else:
+            print('Already exists')
+            return False
+    except OSError: 
+        print("Error: Failed to create the directory.")
+
 
 class DataUploadView(LoginRequiredMixin, View):
     login_url = '/login'
@@ -22,21 +35,40 @@ class DataUploadView(LoginRequiredMixin, View):
     def post(self, request: HttpRequest, *args, **kwargs):
         context = {}
         file = request.FILES.get('file', '')
-        print(file)
+        directory_name = request.POST['directory-name']
+        type = request.POST['inlineRadioOptions']
+        path = f'static/data/{type}/{directory_name}'
+        
         if file == '':
             context['success'] = False
             context['message'] = '파일을 선택해주세요.'
+            return JsonResponse(context, content_type='application/json')
 
         filename = file.name
+
         name_ext = os.path.splitext(filename)[1]
+        if name_ext != '.zip':
+            context['success'] = False
+            context['message'] = 'zip 파일을 넣어주세요.'
+            return JsonResponse(context, content_type='application/json')
 
-        if os.path.isfile('static/' + filename):
-            os.remove('static/' + filename)
+        if createDirectory(path):
+            fs = FileSystemStorage(location=path)
+            fs.save(f"{filename}", file)
+        else:
+            context['success'] = False
+            context['message'] = '이미 존재하는 디렉토리 입니다.'
+            return JsonResponse(context, content_type='application/json')
 
-        fs = FileSystemStorage(location='static')
-
-        name = fs.save(f"{filename}", file)
+        with zipfile.ZipFile(f'{path}/{filename}', 'r') as existing_zip:
+            save_path = f'{path}/{existing_zip.namelist()[0][:-1]}'
+            print(save_path)
+            existing_zip.extractall(f'{path}')
+        os.remove(f'{path}/{filename}')
+        file_list = os.listdir(save_path)
+        file_list = [name for name in file_list if name[0] != "." ] 
+        print(len(file_list)) #개수
 
         context['success'] = True
-        context['message'] = '업로드 성공!.'
+        context['message'] = '업로드 성공!'
         return JsonResponse(context, content_type='application/json')

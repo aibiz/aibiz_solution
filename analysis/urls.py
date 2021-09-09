67b2475bbf898eea205f9eb5bced80a250922d20
kerlic <kerli@naver.com>
2021-09-09 10:51:34 +0900

    Update analysis

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # 모델 학습
     # path('/training', training_views.training_main, name='training')
-    path('analysis', views.analysis_main)
+    path('analysis', views.analysis_main.as_view())
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, JsonResponse
+import os
 
-def analysis_main(request):
-    context = {}
-    return render(request, "analysis.html", context)
+class analysis_main(LoginRequiredMixin, View):
+    login_url = '/login'
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        rootpath = os.getcwd()
+        datapath = rootpath + "/static/data/train_data/aa/recipe1"
+        file_list = os.listdir(datapath)
+        for i in file_list:
+            print(i)
+            data_split = i.split('_')
+        #     # if (data_split[0] > da
+
+        return render(request, "analysis.html", context)
 

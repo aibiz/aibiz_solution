787928dc11fb371515b631e80a541f0271a327f3
kerlic <kerli@naver.com>
2021-09-09 10:51:26 +0900

    Update training

--- training/views.py
+++ training/views.py
@@ -6,6 +6,7 @@ import os
 from django.http import HttpRequest, JsonResponse
 import datetime
 import pandas
+from collections import Counter
 # from aiengine.learning_code import learn_anomaly
 # from aiengine.test_code import test_anomaly
 
@@ -30,11 +31,11 @@ def start_training(request):
     rootpath = '/'.join(rootpath)
 
     trainDataId = rsData['trainDataId']
-    trainStaticPath = mmDataset.objects.filter(id = trainDataId)[0].data_static_path
+    trainStaticPath = mmDataset.objects.filter(id=trainDataId)[0].data_static_path
     trainStaticPath = rootpath + trainStaticPath
 
     testDataId = rsData['testDataId']
-    testStaticPath = mmDataset.objects.filter(id = testDataId)[0].data_static_path
+    testStaticPath = mmDataset.objects.filter(id=testDataId)[0].data_static_path
     testStaticPath = rootpath + testStaticPath
 
     sensorNo = int(rsData['sensorNo'])
@@ -76,13 +77,13 @@ def graphing_training(request):
         file_list = os.listdir(testAnomalyList)
         csv_list = []
         #   파일을 수정시간순으로 정렬
-        for i in range(0, len(file_list)) :
-            for j in range(0, len(file_list)) :
+        for i in range(0, len(file_list)):
+            for j in range(0, len(file_list)):
                 if datetime.datetime.fromtimestamp(os.stat(testAnomalyList + file_list[i]).st_mtime) \
                         < datetime.datetime.fromtimestamp(os.stat(testAnomalyList + file_list[j]).st_mtime) :
                     (file_list[i], file_list[j]) = (file_list[j], file_list[i])
         #   파일 리스트 전체의 csv파일 데이터를 읽어들여와 List 형식으로 변환(전체파일)
-        for k in file_list :
+        for k in file_list:
             data = pandas.read_csv(testAnomalyList + k, header = None)
             data = data.values.tolist()
             csv_list.append([k, data])
@@ -92,26 +93,25 @@ def graphing_training(request):
         #      threshold 표시값 조정
         forAdjustThshld = 1000
         threshold = round(pandas.DataFrame(thresholdpd).loc[0, 0], 5) * forAdjustThshld
-        print("threshold:::", threshold)
         context = {'anomalies_list': file_list, 'csv_list': csv_list, 'current_threshold': threshold,
                    'status_loss': convert_data(trainingStatusFile, 0)[0],
                    'status_val_loss': convert_data(trainingStatusFile, 0)[1],
                    'train_anomaly_score': convert_data(trainingAnomalyFile, 1),
-                   'test_anomaly_score': convert_data(testAnomalyFile, 1), 'state': "True"}
+                   'test_anomaly_score': convert_data(testAnomalyFile, 1),
+                   'state': "True"}
         return JsonResponse(context, content_type='application/json')
-    else :
+    else:
         context['state'] = "False"
         return JsonResponse(context, content_type='application/json')
+
 
 def adjust_threshold(request):
     context = {}
     rsData = json.loads(request.body.decode("utf-8"))
-    print(rsData)
 
     rootpath = os.getcwd()
     trainStaticPath = rsData['trainStaticPath']
     thresholdFile = rootpath + trainStaticPath + "/after_learning/test_input/threshold.txt"
-    print(thresholdFile)
 
     file = open(thresholdFile, 'w')
     adjustThreshold = rsData['adjustThreshold']
@@ -120,14 +120,21 @@ def adjust_threshold(request):
 
     return JsonResponse(context, content_type='application/json')
 
+
 def convert_data(file, mthd):
     data = pandas.read_csv(file, header=None, encoding='cp949')
-    # 1차원 수직행렬의 경우만 mthd = 1
-    if mthd == 1:
-        data.transpose()
-    data = data.values.tolist()
-    if mthd == 1:
-        data = sum(data, [])
+    # anomaly_score의 경우 mthd = 1
+    if mthd == 0:
+        data = data.values.tolist()
+    elif mthd == 1:
+        data = data.iloc[:, 0]
+        data = round(data, 5)
+        # data.transpose()
+        data = data.tolist()
+        data = Counter(data)
+        data = sorted(data.items())
+        data = dict(data)
+        print(data)
     return data
 
 

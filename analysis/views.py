d82387be8eeff318d052d77bc4fc5d0175d9bcda
kerlic <kerli@naver.com>
2021-09-09 18:38:33 +0900

    Update training

--- analysis/views.py
+++ analysis/views.py
@@ -20,6 +20,10 @@ class analysis_main(LoginRequiredMixin, View):
         formatted_startdate = time.strptime(date_start,"%Y-%m-%d")
 
         rootpath = os.getcwd()
+
+        # 여러 챔버, 장비 선택시 for문 시작점
+        # ex for path in sected_list:
+        #       datapath = rootpath + path
         datapath = rootpath + "/static/data/train_data/aa/recipe1"
         file_list = os.listdir(datapath)
         file_list_csv= [file for file in file_list if file.endswith(".csv")]
@@ -31,7 +35,6 @@ class analysis_main(LoginRequiredMixin, View):
             formatted_file_date = time.strptime(date, "%Y_%m_%d")
             if (formatted_startdate <= formatted_file_date and formatted_file_date <= formatted_enddate):
                 filtered_filelist.append(i)
-        # print(filtered_filelist)
 
         # for i in range(0, len(filtered_filelist)):
         #     for j in range(0, len(filtered_filelist)):
@@ -40,20 +43,26 @@ class analysis_main(LoginRequiredMixin, View):
         #             print(filtered_filelist[i])
         #             (filtered_filelist[i], filtered_filelist[j]) = (filtered_filelist[j], filtered_filelist[i])
         print(filtered_filelist)
+
+        sensor_list = []
         csv_data = []
+
         for k in filtered_filelist :
             data = pandas.read_csv(datapath +'/'+ k, header = None)
-            rdata = data.iloc[:, 2]
-            rdata = rdata.values.transpose().tolist()
-            rdata = np.reshape(rdata, -1)
-            break
-            csv_data.append([k, data])
+            for sensor_num in sensor_list:
+                idata = data.iloc[:, sensor_num]
+
+                d1_data = idata.values.transpose().tolist()
+                d1_data = np.reshape(d1_data, -1)
+
+                csv_data = idata.values.transpose.tolist()
+                csv_data.append([k, data])
         print(data)
-        mn = np.mean(rdata)
-        std = np.std(rdata)
-        print("mean:::",mn)
+        mn = np.mean(d1_data)
+        std = np.std(d1_data)
+        print("mean:::", mn)
         print("std:::", std)
-        for j in rdata:
+        for j in d1_data:
             print((j - mn)/std)
         # print("std!!!!!!!!!!", np.std(csv_data))
         # print(csv_data)
--- training/views.py
+++ training/views.py
@@ -100,7 +100,6 @@ def graphing_training(request):
                    'train_anomaly_score': convert_data(trainingAnomalyFile, 1),
                    'test_anomaly_score': convert_data(testAnomalyFile, 1),
                    'state': "True"}
-        print("html-----------------")
         # print("!!!!!!!!!!!!!!!!!!!!!!", context['csv_list'])
         return JsonResponse(context, content_type='application/json')
     else:

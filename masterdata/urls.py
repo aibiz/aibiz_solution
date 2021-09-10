e5b52e4ca9eb1617d7e08bbe329272e0b4e34ae0
takaharu0 <hjkbnm2@naver.com>
2021-09-10 15:57:31 +0900

    Master Data Update

--- masterdata/urls.py
+++ masterdata/urls.py
@@ -3,8 +3,18 @@ from . import views
 
 urlpatterns = [
     path('master/', views.masterdatamain.as_view(), name='mastermain'),
-    path('master/equipfind', views.masterdataequipfind.as_view(), name='mastermainequipfind'),
+    path('master/recipefind', views.masterdatarecipefind.as_view(), name='mastermainrecipefind'),
     path('master/problemfind', views.masterdataproblemfind.as_view(), name='mastermainproblemfind'),
     path('master/datasetfind', views.masterdatadatasetfind.as_view(), name='mastermaindatasetfind'),
-    path('master/equipmodify', views.equipmentdatamodify.as_view(), name='equipmodify')
+    #mm_model
+    path('master/equipmodify', views.equipmentdatamodify.as_view(), name='equipmodify'),
+    path('master/updateequip', views.update_equipment.as_view(), name='updateequip'),
+    #mm_problem
+    path('master/problemmodify', views.problemdatamodify.as_view(), name='problemmodify'),
+    path('master/updateproblem', views.update_problem.as_view(), name='updateproblem'),
+    path('master/deleteproblem', views.delete_problem.as_view(), name='deleteproblem'),
+    #mm_dataset
+    path('master/datasetmodify', views.datasetdatamodify.as_view(), name='datasetmodify'),
+    path('master/updatedataset', views.update_dataset.as_view(), name='updatedataset'),
+    path('master/deletedataset', views.delete_dataset.as_view(), name='deletedataset')
 ]
--- masterdata/views.py
+++ masterdata/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpRequest, JsonResponse
-from config.models import mmModel, mmProblem, mmDataset
+from config.models import mmRecipe, mmProblem, mmDataset
 
 class masterdatamain(View):
     def get(self, request: HttpRequest, *args, **kwargs):
@@ -9,11 +9,11 @@ class masterdatamain(View):
 
         return render(request, 'masterdata/masterdatamain.html', context)
 
-class masterdataequipfind(View) :
+class masterdatarecipefind(View) :
     def get(self, request: HttpRequest, *args, **kwargs):
         context = {}
 
-        data = mmModel.objects.all()
+        data = mmRecipe.objects.all()
         print(data.values())
         context['data'] = list(data.values())
 
@@ -45,11 +45,137 @@ class equipmentdatamodify(View):
 
         id = request.GET['id']
         data = mmModel.objects.get(id=id)
-        print(data)
+
         context = {
             'data' : data
         }
 
         return render(request, 'masterdata/equipdatamodify.html', context)
+
+class update_equipment(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        print(request.POST)
+        id = request.POST['id']
+        id = request.POST['id']
+
+        data = mmDataset.objects.all()
+        print(data.values())
+        context['data'] = list(data.values())
+
+        return JsonResponse(context, content_type='application/json')
+
+class problemdatamodify(View):
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.GET['id']
+        data = mmProblem.objects.get(id=id)
+
+        context = {
+            'data' : data
+        }
+
+        return render(request, 'masterdata/problemmodify.html', context)
+
+class update_problem(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.POST['id']
+        problem_name = request.POST['problem_name']
+        problem_note = request.POST['problem_note']
+
+        try:
+            data = mmProblem.objects.get(id=id)
+            data.problem_name = problem_name
+            data.problem_note = problem_note
+            data.save()
+        except:
+            context['success'] = False
+            context['message'] = "수정에 실패하였습니다."
+            return JsonResponse(context, content_type='application/json')
+
+        context['success'] = True
+        context['message'] = "수정되었습니다."
+
+        return JsonResponse(context, content_type='application/json')
+
+class delete_problem(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.POST['id']
+
+        try:
+            data = mmProblem.objects.get(id=id)
+            data.delete()
+        except:
+            context['success'] = False
+            context['message'] = "삭제에 실패하였습니다."
+            return JsonResponse(context, content_type='application/json')
+
+        context['success'] = True
+        context['message'] = "삭제되었습니다."
+
+        return JsonResponse(context, content_type='application/json')
+
+class datasetdatamodify(View):
+    def get(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.GET['id']
+        data = mmDataset.objects.get(id=id)
+
+        context = {
+            'data' : data
+        }
+
+        return render(request, 'masterdata/datasetmodify.html', context)
+
+class update_dataset(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.POST['id']
+
+        try:
+            data = mmDataset.objects.get(id=id)
+            data.equip_name = request.POST['equip_name']
+            data.chamber_name = request.POST['chamber_name']
+            data.recipe_name = request.POST['recipe_name']
+            data.data_static_path = request.POST['data_static_path']
+            data.purpose = request.POST['purpose']
+            data.data_name = request.POST['data_name']
+            data.save()
+        except:
+            context['success'] = False
+            context['message'] = "수정에 실패하였습니다."
+            return JsonResponse(context, content_type='application/json')
+
+        context['success'] = True
+        context['message'] = "수정되었습니다."
+
+        return JsonResponse(context, content_type='application/json')
+
+class delete_dataset(View):
+    def post(self, request: HttpRequest, *args, **kwargs):
+        context = {}
+
+        id = request.POST['id']
+
+        try:
+            data = mmDataset.objects.get(id=id)
+            data.delete()
+        except:
+            context['success'] = False
+            context['message'] = "삭제에 실패하였습니다."
+            return JsonResponse(context, content_type='application/json')
+
+        context['success'] = True
+        context['message'] = "삭제되었습니다."
+
+        return JsonResponse(context, content_type='application/json')
 
 

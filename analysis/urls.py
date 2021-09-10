d022a737d10c40bd724fcf9f9d9d3bf598ed18ce
kerlic <kerli@naver.com>
2021-09-11 04:13:55 +0900

    Update analysis

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     # 모델 학습
     # path('/training', training_views.training_main, name='training')
-    path('analysis', views.analysis_main.as_view())
+    path('analysis', views.analysis_main.as_view()),
+    path('analysis/find_chamber', views.find_chamber.as_view()),
+    path('analysis/find_recipe', views.find_recipe.as_view()),
+    path('analysis/find_revision', views.find_revision.as_view()),
+    path('analysis/find_sensor', views.find_sensor.as_view())
 ]

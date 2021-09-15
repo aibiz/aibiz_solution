d149a819a7b6bd6ac16b67a95f7d13250f0ece3f
kerlic <kerli@naver.com>
2021-09-15 11:23:20 +0900

    Update analysis

--- analysis/views.py
+++ analysis/views.py
@@ -32,55 +32,63 @@ class analysis_main(LoginRequiredMixin, View):
         sensor_list = [2] #넘겨받아 split
 
         print("tree selected::::::", request.GET.get('tree_checked_ids'))
+        tree_selected = request.GET.get('tree_checked_ids')
+        # 파일 경로 및 파일명에는 ","가 들어가면 안됨
+        tree_selected_list = tree_selected.split(",")
+        print(tree_selected_list)
 
-        rootpath = os.getcwd()
+        for selected_sensor in tree_selected_list:
+            temp = selected_sensor.split('#')
+            datapath = temp[0]
+            sensor_list = temp[1]
 
-        # 여러 챔버, 장비 선택시 for문 시작점
-        # ex for path in selected_list:
-        #       datapath = rootpath + path
-        datapath = rootpath + "/static/data/eq1_ch11_recipe1_v1/train_data/data1" + '/' #server
-        file_list = os.listdir(datapath)
-        file_list_csv = [file for file in file_list if file.endswith(".csv")]
-        filtered_filelist = []
+            rootpath = os.getcwd()
+            # 여러 챔버, 장비 선택시 for문 시작점
+            # ex for path in selected_list:
+            #       datapath = rootpath + path
+            datapath = rootpath + datapath + '/' #server
+            file_list = os.listdir(datapath)
+            file_list_csv = [file for file in file_list if file.endswith(".csv")]
+            filtered_filelist = []
 
-        # 기간내 파일 필터링
-        for i in file_list_csv:
-            date = i[:10]
-            formatted_file_date = time.strptime(date, "%Y_%m_%d")
-            if (formatted_startdate <= formatted_file_date and formatted_file_date <= formatted_enddate):
-                filtered_filelist.append(i)
+            # 기간내 파일 필터링
+            for i in file_list_csv:
+                date = i[:10]
+                formatted_file_date = time.strptime(date, "%Y_%m_%d")
+                if (formatted_startdate <= formatted_file_date and formatted_file_date <= formatted_enddate):
+                    filtered_filelist.append(i)
 
-        d1_datasum = []
-        csv_data = []
-        for k in filtered_filelist:
-            data = pandas.read_csv(datapath + k, header=None)
-            for sensor_num in sensor_list:
-                sensor_data = data.iloc[:, sensor_num]
-                sensor_data = sensor_data.values.transpose().tolist()
-                d1_data = np.reshape(sensor_data, -1)
-                d1_datasum.extend(d1_data)
-                csv_data.append([k, sensor_data])
-        # end for selected_list
+            d1_datasum = []
+            csv_data = []
+            for k in filtered_filelist:
+                data = pandas.read_csv(datapath + k, header=None)
+                for sensor_num in sensor_list:
+                    sensor_data = data.iloc[:, sensor_num]
+                    sensor_data = sensor_data.values.transpose().tolist()
+                    d1_data = np.reshape(sensor_data, -1)
+                    d1_datasum.extend(d1_data)
+                    csv_data.append([k, sensor_data])
+            # end for selected_list
 
-        #anomaly 데이터처리
-        anomaly_path = rootpath + "/static/data/monitoring_anomalies" + '/'
-        anomaly_file_list = os.listdir(anomaly_path)
-        anomaly_csv_data = []
+            #anomaly 데이터처리
+            anomaly_path = rootpath + "/static/data/monitoring_anomalies" + '/'
+            anomaly_file_list = os.listdir(anomaly_path)
+            anomaly_csv_data = []
 
-        #   파일을 수정시간순으로 정렬
-        for i in range(0, len(anomaly_file_list)):
-            for j in range(0, len(anomaly_file_list)):
-                if datetime.fromtimestamp(os.stat(anomaly_path + anomaly_file_list[i]).st_mtime) \
-                        < datetime.fromtimestamp(os.stat(anomaly_path + anomaly_file_list[j]).st_mtime):
-                    (anomaly_file_list[i], anomaly_file_list[j]) = (anomaly_file_list[j], anomaly_file_list[i])
-        #   파일 리스트 전체의 csv파일 데이터를 읽어들여와 List 형식으로 변환(전체파일)
-        for k in anomaly_file_list:
-            anomaly_data = pandas.read_csv(anomaly_path + k, header=None, )
-            anomaly_data = anomaly_data.values.tolist()
-            anomaly_csv_data.append(anomaly_data[0])
-            anomaly_csv_data.append(anomaly_data[1])
-            d1_datasum.extend(np.reshape(anomaly_data[0], -1))
-            d1_datasum.extend(np.reshape(anomaly_data[1], -1))
+            #   파일을 수정시간순으로 정렬
+            for i in range(0, len(anomaly_file_list)):
+                for j in range(0, len(anomaly_file_list)):
+                    if datetime.fromtimestamp(os.stat(anomaly_path + anomaly_file_list[i]).st_mtime) \
+                            < datetime.fromtimestamp(os.stat(anomaly_path + anomaly_file_list[j]).st_mtime):
+                        (anomaly_file_list[i], anomaly_file_list[j]) = (anomaly_file_list[j], anomaly_file_list[i])
+            #   파일 리스트 전체의 csv파일 데이터를 읽어들여와 List 형식으로 변환(전체파일)
+            for k in anomaly_file_list:
+                anomaly_data = pandas.read_csv(anomaly_path + k, header=None, )
+                anomaly_data = anomaly_data.values.tolist()
+                anomaly_csv_data.append(anomaly_data[0])
+                anomaly_csv_data.append(anomaly_data[1])
+                d1_datasum.extend(np.reshape(anomaly_data[0], -1))
+                d1_datasum.extend(np.reshape(anomaly_data[1], -1))
 
         # mean, std
         mean_val = np.mean(d1_datasum)
